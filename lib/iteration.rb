cca217eda1424c0752978c086df2da882c9342d8
Adam  Shappy <ajs11300@gmail.com>
2019-12-21 21:50:35 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -4,14 +4,33 @@ def join_ingredients(src)
   # Build a new Array that contains strings where each pair of foods is
   # inserted into this template:
   #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
+  
+  row_index = 0
+  a = []
+  while row_index < src.count do
+      a << "I love #{src[row_index][0]} and #{src[row_index][1]} on my pizza"
+      row_index +=1 
+  end
+  a 
+  # "I love (inner array element 0) and (inner array element 1) on my pizza"
   # As such, there should be a new String for each inner array, or pair
+  
+  
 end
 
 def find_greater_pair(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
+  row_index = 0
+  a = []
+  while row_index < src.count do 
+    max_number_array = src[row_index].max(1)
+    number_return = max_number_array[0]
+    a << number_return 
+    row_index +=1
+  end 
+  a
 end
 
 def total_even_pairs(src)
@@ -22,4 +41,16 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
+  row_index = 0 
+  total = 0 
+  while row_index < src.count do 
+    if src[row_index][0]%2 == 0 && src[row_index][1]%2 == 0
+      pair_total = src[row_index][0] + src[row_index][1]
+      total += pair_total
+      row_index += 1 
+    else 
+      row_index += 1
+    end 
+  end 
+  total 
 end
