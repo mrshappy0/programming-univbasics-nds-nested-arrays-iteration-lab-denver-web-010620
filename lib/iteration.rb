dd69e64a12f83ace4fd8e2ee07babb585adb565e
Adam  Shappy <ajs11300@gmail.com>
2019-12-21 21:27:31 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -7,7 +7,7 @@ def join_ingredients(src)
   
   row_index = 0
   while row_index < src.count do
-      return "I love #{src[row_index][0]} and #{src[row_index][1]} on my pizza"
+      puts "I love #{src[row_index][0]} and #{src[row_index][1]} on my pizza"
       row_index +=1 
   end
   
